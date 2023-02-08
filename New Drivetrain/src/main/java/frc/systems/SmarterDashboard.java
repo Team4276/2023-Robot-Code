cc4fbacece9af5432d7813134d67b66caa681013
Breadfest <ethanoskatt1@gmail.com>
2023-02-07 16:25:53 -0800

    Rearchitectured Drivetrain

---
+++ New Drivetrain/src/main/java/frc/systems/SmarterDashboard.java
@@ -0,0 +1,19 @@
+package frc.systems;
+
+import javax.swing.JFrame;
+
+public class SmarterDashboard extends JFrame {
+    public static void main(){
+        SmarterDashboard gui = new SmarterDashboard();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setSize(200,200);
+        gui.setVisible(true);
+        gui.setTitle("SmarterDashboard");
+
+    }
+
+
+
+
+    
+}
