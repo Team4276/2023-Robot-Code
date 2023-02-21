104d048d7dc8f66cf0b5841eca3b0ca39cd776a0
AndyC <andrew.cappon@gmail.com>
2023-02-20 17:52:24 -0800

    Add camera server for driver cam

--- master/src/main/java/frc/robot/Robot.java
+++ master/src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Notifier;
@@ -32,6 +33,8 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
  
+    CameraServer.startAutomaticCapture();
+
     leftJoystick = new Joystick(0);
     rightJoystick = new Joystick(1);
     xboxJoystick = new Joystick(2);
