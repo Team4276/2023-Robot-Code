5b5732af64458d42ac718302a4dc0e1deb417530
AndyC <andrew.cappon@gmail.com>
2023-02-22 17:23:42 -0800

    Command zero power to elbow and shoulder at init time so nothing moves when first enabled

--- NewerDrivetrain/src/main/java/frc/systems/Shoulder.java
+++ NewerDrivetrain/src/main/java/frc/systems/Shoulder.java
@@ -12,16 +12,18 @@ public class Shoulder {
     private static CANSparkMax driveShoulder_L;
     private double deadband = 0.05;
 
-
     public Shoulder(int port_R, int port_L) {
         driveShoulder_R = new CANSparkMax(port_R, MotorType.kBrushless);
         driveShoulder_L = new CANSparkMax(port_L, MotorType.kBrushless);
-    }    
+        setShoulderSpeed_R(0);
+        setShoulderSpeed_L(0);
+    }
 
     // Speed inrange -1.0 to +1.0
     public void setShoulderSpeed_R(double speed) {
         driveShoulder_R.set(speed);
-     }
+    }
+
     public void setShoulderSpeed_L(double speed) {
         driveShoulder_L.set(speed);
     }
@@ -29,11 +31,11 @@ public class Shoulder {
     public void updatePeriodic() {
         if (Math.abs(Robot.xboxController.getLeftY()) > deadband) {
             leftY = Math.pow(Robot.xboxController.getLeftY(), 3 / 2);
-            Robot.mShoulder.setShoulderSpeed_R(-1*leftY);
-            Robot.mShoulder.setShoulderSpeed_L(leftY);
+            setShoulderSpeed_R(-1 * leftY);
+            setShoulderSpeed_L(leftY);
         } else {
-            Robot.mShoulder.setShoulderSpeed_R(0);
-            Robot.mShoulder.setShoulderSpeed_L(0);
+            setShoulderSpeed_R(0);
+            setShoulderSpeed_L(0);
         }
 
         SmartDashboard.putNumber("Shoulder Power", leftY);
