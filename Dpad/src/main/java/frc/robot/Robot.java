e9b42036d5c259e78b2dc3784ceb543b2c58ef74
JonathanG10 <jhgertz100@student.hbuhsd.edu>
2023-02-25 10:18:28 -0800

    Dpad PID setpoints (not tested)

---
+++ Dpad/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,160 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.systems.Balance;
+import frc.systems.Elbow;
+import frc.systems.PIDElbow;
+import frc.systems.PIDDrivetrain;
+import frc.systems.Shoulder;
+import frc.systems.TeleopDrivetrain;
+import frc.utilities.Gyroscope;
+import frc.utilities.RoboRioPorts;
+import frc.utilities.Xbox;
+
+public class Robot extends TimedRobot {
+
+  public static Joystick leftJoystick;
+  public static Joystick rightJoystick;
+  public static XboxController xboxController;
+
+  Notifier driveRateGroup;
+  public static TeleopDrivetrain mTeleopDrivetrain;
+  public static PIDDrivetrain mPIDDrivetrain;
+
+  public static Shoulder mShoulder;
+  public static Elbow mElbow;
+
+  public static Timer systemTimer;
+
+  public static double initialPitch = 0;
+
+  public static boolean isCAN = true;
+  public static boolean usingPIDDrivetrain = false;
+
+  public static double pov;
+
+  public static void timedDrive() {
+    if (Robot.xboxController.getRawButton(Xbox.B)) {
+      Robot.usingPIDDrivetrain = true;
+      PIDDrivetrain.PIDDrivetrainUpdate();
+      Balance.balance(Gyroscope.GetCorrectPitch(Gyroscope.GetPitch()));
+      PIDDrivetrain.updateTelemetry();
+    } else if (Robot.xboxController.getRawButton(Xbox.X)) {
+      PIDDrivetrain.holdPosition = true;
+      PIDDrivetrain.PIDDrivetrainUpdate();
+    } else {
+      Robot.usingPIDDrivetrain = false;
+      Balance.stopBalance();
+      mTeleopDrivetrain.operatorDrive();
+      TeleopDrivetrain.updateTelemetry();
+    }
+
+    mShoulder.updatePeriodic();
+    mElbow.updatePeriodic();
+    
+    PIDElbow.PIDElbowUpdate();
+    PIDElbow.updateTelemetry();
+  }
+
+
+  /**
+   * This function is run when the robot is first started up and should be used
+   * for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+
+    CameraServer.startAutomaticCapture();
+
+    leftJoystick = new Joystick(0);
+    rightJoystick = new Joystick(1);
+    xboxController = new XboxController(2);
+
+    mTeleopDrivetrain = new TeleopDrivetrain(RoboRioPorts.CAN_DRIVE_L1, RoboRioPorts.CAN_DRIVE_L2,
+        RoboRioPorts.CAN_DRIVE_R1, RoboRioPorts.CAN_DRIVE_R2);
+
+    mPIDDrivetrain = new PIDDrivetrain(RoboRioPorts.CAN_DRIVE_L1, RoboRioPorts.CAN_DRIVE_L2,
+        RoboRioPorts.CAN_DRIVE_R1, RoboRioPorts.CAN_DRIVE_R2);
+
+    PIDDrivetrain.PIDDrivetrainInit();
+
+        mShoulder = new Shoulder(RoboRioPorts.CAN_SHOULDER_R, RoboRioPorts.CAN_SHOULDER_L);
+        mElbow = new Elbow(RoboRioPorts.CAN_ELBOW);
+   
+    // Drive train motor control is done on its own timer driven thread regardless
+    // of disabled/teleop/auto mode selection
+    driveRateGroup = new Notifier(Robot::timedDrive);
+    driveRateGroup.startPeriodic(0.05);
+  }
+
+  /**
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and
+   * test.
+   *
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    Gyroscope.gyroscopeUpdate();
+    SmartDashboard.putBoolean("Using Joystick", TeleopDrivetrain.usingJoystick);
+    pov = xboxController.getPOV();
+    SmartDashboard.putNumber("POV", pov);
+  }
+
+  @Override
+  public void autonomousInit() {
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {
+    initialPitch = Gyroscope.GetPitch();
+    PIDElbow.PIDElbowInit();
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+  }
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {
+  }
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {
+  }
+}
---
+++ Dpad/src/main/java/frc/systems/PIDElbow.java
@@ -0,0 +1,190 @@
+package frc.systems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
+import frc.utilities.Xbox;
+
+public class PIDElbow extends Elbow {
+
+    public PIDElbow(int port) {
+        super(port);
+        //TODO Auto-generated constructor stub
+    }
+
+    // PID coefficients
+    private static double kP = 5e-5;
+    private static double kI = 1e-6;
+    private static double kD = 0;
+    private static double kIz = 0;
+    private static double kFF = 0.000156;
+    private static double kMaxOutput = 1;
+    private static double kMinOutput = -1;
+
+    // Smart Motion Coefficients
+    private static double maxVel = 1000; // rpm
+    private static double maxAcc = 100;
+    private static double minVel = 0;
+
+    private static double allowedErr = 0;
+    private static double deadband = 0.05;
+
+    public static boolean modeIsSetPosition = false; // Otherwise set velocity
+
+    public static double setPoint_W;
+
+    public static void setModePosition() {
+        modeIsSetPosition = true;
+    }
+
+    public static void setModeVelocity() {
+        modeIsSetPosition = false;
+    }
+
+    private static void setPIDReference(double setPoint_W){
+        SmartDashboard.putNumber("Set Position", setPoint_W);
+        driveElbow.getPIDController().setReference(setPoint_W, CANSparkMax.ControlType.kSmartMotion);
+    }
+
+    public static void PIDElbowInit() {
+        int smartMotionSlot = 0;
+
+        CANSparkMax[] motorArray = { driveElbow };
+        for (CANSparkMax motor : motorArray) {
+            SparkMaxPIDController pidController = motor.getPIDController();
+
+            pidController.setP(kP);
+            pidController.setI(kI);
+            pidController.setD(kD);
+            pidController.setIZone(kIz);
+            pidController.setFF(kFF);
+            pidController.setOutputRange(kMinOutput, kMaxOutput);
+            pidController.setSmartMotionMaxVelocity(maxVel, smartMotionSlot);
+            pidController.setSmartMotionMinOutputVelocity(minVel, smartMotionSlot);
+            pidController.setSmartMotionMaxAccel(maxAcc, smartMotionSlot);
+            pidController.setSmartMotionAllowedClosedLoopError(allowedErr, smartMotionSlot);
+        }
+
+        // display PID coefficients on SmartDashboard
+        SmartDashboard.putNumber("P Gain", kP);
+        SmartDashboard.putNumber("I Gain", kI);
+        SmartDashboard.putNumber("D Gain", kD);
+        SmartDashboard.putNumber("I Zone", kIz);
+        SmartDashboard.putNumber("Feed Forward", kFF);
+        SmartDashboard.putNumber("Max Output", kMaxOutput);
+        SmartDashboard.putNumber("Min Output", kMinOutput);
+
+        // display Smart Motion coefficients
+        SmartDashboard.putNumber("Max Velocity", maxVel);
+        SmartDashboard.putNumber("Min Velocity", minVel);
+        SmartDashboard.putNumber("Max Acceleration", maxAcc);
+        SmartDashboard.putNumber("Allowed Closed Loop Error", allowedErr);
+        SmartDashboard.putNumber("Set Position", 0);
+        SmartDashboard.putNumber("Set Velocity", 0);
+
+        // button to toggle between velocity and smart motion modes
+        SmartDashboard.putBoolean("Mode", true);
+
+        // display POV coefficient 
+        SmartDashboard.putNumber("POV", Robot.xboxController.getPOV());
+    }
+
+    public static void PIDElbowUpdate() {
+        if ((Math.abs(Robot.xboxController.getRightY()) > deadband)) {
+            setModeVelocity();
+        } else {
+            setModePosition();
+        }
+
+        if (modeIsSetPosition) {
+            if (Robot.pov != -1){
+
+                if (Xbox.POVup == Robot.pov){
+                    setPoint_W = 7;
+                    setPIDReference(setPoint_W);
+                } else if (Xbox.POVdown == Robot.pov){
+                    setPoint_W = -7;
+                    setPIDReference(setPoint_W);
+                } else if (Xbox.POVright == Robot.pov){
+                    setPoint_W = 2;
+                    setPIDReference(setPoint_W);
+                } else if (Xbox.POVleft == Robot.pov){
+                    setPoint_W = -2;
+                    setPIDReference(setPoint_W);
+                }
+            }
+        }
+
+        Update(driveElbow);
+    }
+
+    public static void Update(CANSparkMax motor) {
+        SparkMaxPIDController pidController = motor.getPIDController();
+        
+        double p = SmartDashboard.getNumber("P Gain", 0);
+        double i = SmartDashboard.getNumber("I Gain", 0);
+        double d = SmartDashboard.getNumber("D Gain", 0);
+        double iz = SmartDashboard.getNumber("I Zone", 0);
+        double ff = SmartDashboard.getNumber("Feed Forward", 0);
+        double max = SmartDashboard.getNumber("Max Output", 0);
+        double min = SmartDashboard.getNumber("Min Output", 0);
+        double maxV = SmartDashboard.getNumber("Max Velocity", 0);
+        double minV = SmartDashboard.getNumber("Min Velocity", 0);
+        double maxA = SmartDashboard.getNumber("Max Acceleration", 0);
+        double allE = SmartDashboard.getNumber("Allowed Closed Loop Error", 0);
+
+        if ((p != kP)) {
+            pidController.setP(p);
+            kP = p;
+        }
+        if ((i != kI)) {
+            pidController.setI(i);
+            kI = i;
+        }
+        if ((d != kD)) {
+            pidController.setD(d);
+            kD = d;
+        }
+        if ((iz != kIz)) {
+            pidController.setIZone(iz);
+            kIz = iz;
+        }
+        if ((ff != kFF)) {
+            pidController.setFF(ff);
+            kFF = ff;
+        }
+        if ((max != kMaxOutput) || (min != kMinOutput)) {
+            pidController.setOutputRange(min, max);
+            kMinOutput = min;
+            kMaxOutput = max;
+        }
+        if ((maxV != maxVel)) {
+            pidController.setSmartMotionMaxVelocity(maxV, 0);
+            maxVel = maxV;
+        }
+        if ((minV != minVel)) {
+            pidController.setSmartMotionMinOutputVelocity(minV, 0);
+            minVel = minV;
+        }
+        if ((maxA != maxAcc)) {
+            pidController.setSmartMotionMaxAccel(maxA, 0);
+            maxAcc = maxA;
+        }
+        if ((allE != allowedErr)) {
+            pidController.setSmartMotionAllowedClosedLoopError(allE, 0);
+            allowedErr = allE;
+        }
+    }
+
+    public static void updateTelemetry(){
+        SmartDashboard.putNumber("Wrist Encoder", driveElbow.getEncoder().getPosition());
+        
+        SmartDashboard.putNumber("SetPoint_W_Pos", setPoint_W);
+
+        SmartDashboard.putNumber("Encoder_W_Pos", driveElbow.getEncoder().getPosition());
+
+        SmartDashboard.putNumber("MotorOutput_W_Pos", driveElbow.getAppliedOutput());
+    }
+}
