72bd12c5345adbe891880f463d60db5676158458
acappon1 <acappon1@hotmail.com>
2023-03-20 23:14:06 -0700

    Write to log when moving, don't fill log when sitting still Max total size for log files is 3MB

--- andy_master/src/main/java/frc/utilities/LogFile.java
+++ andy_master/src/main/java/frc/utilities/LogFile.java
@@ -7,6 +7,11 @@ import java.time.LocalTime;
 
 public class LogFile {
 
+    private final int MAX_NUMBER_OF_LOG_FILES = 10;
+    private final int MAX_SIZE_OF_ONE_LOG_FILE = 300000; // 10 * 300KB approx 3MB total max for log files
+
+    private int writeCount = 0;
+    private String path;
     private FileWriter logFileWriter;
 
     public LogFile() {
@@ -19,10 +24,10 @@ public class LogFile {
 
             // Limit number of files to 10 - delete oldest if needed
             long oldestFileTime = Long.MAX_VALUE;
-            ;
+
             File oldestFile = null;
             File targetDir = new File("/home/admin/");
-            if (targetDir.list().length > 9) {
+            if (targetDir.list().length > MAX_NUMBER_OF_LOG_FILES - 1) {
                 File[] files = targetDir.listFiles();
                 for (File file : files) {
                     if (file.lastModified() < oldestFileTime) {
@@ -35,7 +40,8 @@ public class LogFile {
                 }
             }
 
-            logFileWriter = new FileWriter("/home/admin/logpos_" + currentTime.toString() + ".txt");
+            path = "/home/admin/logpos_" + currentTime.toString() + ".txt";
+            logFileWriter = new FileWriter(path);
 
         } catch (Exception e) {
             return false;
@@ -45,6 +51,15 @@ public class LogFile {
 
     public boolean write(String val) {
         try {
+            writeCount++;
+            if (writeCount > 100) {
+                writeCount = 0;
+                File file = new File(path);
+                if (file.length() > MAX_SIZE_OF_ONE_LOG_FILE) {
+                    logFileWriter.close();
+                    init();
+                }
+            }
             logFileWriter.write(val);
         } catch (IOException e) {
             return false;
