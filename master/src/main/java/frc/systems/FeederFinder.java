a9ab640a2da2424e7e411b21d125c60d716f1f37
AndyC <andrew.cappon@gmail.com>
2023-03-25 09:21:14 -0700

    First version of Feeder Finder that seems to try to follow a line. Need to test on the practice field

--- master/src/main/java/frc/systems/FeederFinder.java
+++ master/src/main/java/frc/systems/FeederFinder.java
@@ -9,16 +9,10 @@ public class FeederFinder {
 
     private static final int idApriltagOnHumanPlayerBoard = 14;
 
-    private static final double stopRangeFeet = 1.5; // Distance from camera to the front of the bumper
-    private static final double slowRangeFeet = stopRangeFeet + 1.0;
-    private static final double medRangeFeet = stopRangeFeet + 2.0;
-
-    private static final double fastPower = 0.9;
-    private static final double medPower = 0.6;
-    private static final double slowPower = 0.2;
+    private static final double medPower = 0.3;
+    private static final double slowPower = 0.1;
 
     private static final double lineFollowerDeadZone = 0.1;
-    private static final double lineFollowerSlowZone = 0.1;
 
     private static boolean isValidApriltagPosition = false;
     private static Vector3 v3ApriltagPositionInRobotRelativeCoordinates;
@@ -46,29 +40,16 @@ public class FeederFinder {
                 // the Apriltag on the human player board
                 SmartDashboard.putNumber("APriltag14_X", v3ApriltagPositionInRobotRelativeCoordinates.x);
                 SmartDashboard.putNumber("APriltag14_Y", v3ApriltagPositionInRobotRelativeCoordinates.y);
-                if (v3ApriltagPositionInRobotRelativeCoordinates.y > stopRangeFeet) {
-                    power_R = slowPower;
-                    power_L = slowPower;
-                    if (v3ApriltagPositionInRobotRelativeCoordinates.y > slowRangeFeet) {
-                        power_R = medPower;
-                        power_L = medPower;
-                    } else if (v3ApriltagPositionInRobotRelativeCoordinates.y > medRangeFeet) {
-                        power_R = fastPower;
-                        power_L = fastPower;
-                    }
 
-                    if (v3ApriltagPositionInRobotRelativeCoordinates.x > lineFollowerDeadZone) {
-                        power_R += slowPower;
-                    } else if (v3ApriltagPositionInRobotRelativeCoordinates.x > lineFollowerSlowZone) {
-                        power_R += medPower;
-                    }
+                power_R = medPower;
+                power_L = medPower;
 
-                    if (v3ApriltagPositionInRobotRelativeCoordinates.x < (-1 * lineFollowerDeadZone)) {
-                        power_L += slowPower;
-                    } else if (v3ApriltagPositionInRobotRelativeCoordinates.x < (-1 * lineFollowerSlowZone)) {
-                        power_L += medPower;
-                    }
+                if (v3ApriltagPositionInRobotRelativeCoordinates.x < lineFollowerDeadZone) {
+                    power_L += slowPower;
+                } else if (v3ApriltagPositionInRobotRelativeCoordinates.x < (-1 * lineFollowerDeadZone)) {
+                    power_R += slowPower;
                 }
+
             } else {
                 // It is expected that the Limelight wil lose sight of the Apriltag as it gets
                 // close to the feeder
@@ -102,12 +83,12 @@ public class FeederFinder {
             z = positionLimelight[2];
 
             // position units are meters at this point - convert to feet
-            //x *= feet_per_meter;
-            //y *= feet_per_meter;
-            //z *= feet_per_meter;
+            x *= feet_per_meter;
+            y *= feet_per_meter;
+            z *= feet_per_meter;
 
             // Limelight is offset to the right of the robot centerline
-            x -= 0.5; // TODO measure on the robot Units are feet
+            // x -= 0.5; // TODO measure on the robot Units are feet
 
             v3ApriltagPositionInRobotRelativeCoordinates.set(x, y, z);
             return true;
