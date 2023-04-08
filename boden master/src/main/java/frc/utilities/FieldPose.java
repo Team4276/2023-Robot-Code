10198643a6cc0f62638e2e5baf72140ffcbecda0
AndyC <andrew.cappon@gmail.com>
2023-04-08 12:18:17 -0700

    getEstimatedGlobalPose() now returns Pose2d

--- boden master/src/main/java/frc/utilities/FieldPose.java
+++ boden master/src/main/java/frc/utilities/FieldPose.java
@@ -1,5 +1,6 @@
 
 package frc.utilities;
+
 import java.io.IOException;
 import java.util.Optional;
 
@@ -18,20 +19,32 @@ import edu.wpi.first.math.geometry.Translation3d;
 
 public class FieldPose {
 
-    public static Optional<EstimatedRobotPose> pose;
-    public static void initiatePose () throws IOException{
+    private static PhotonCamera cam;
+    private static PhotonPoseEstimator photonPoseEstimator;
+    Optional<EstimatedRobotPose> pose;
+
+    FieldPose() {
+        cam = new PhotonCamera("testCamera");
+
+         try {
+            AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
+            Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
+            photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, cam, robotToCam);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
-     public static void getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) throws IOException {
-        PhotonCamera cam = new PhotonCamera("testCamera");
-        AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
-        Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0)); 
-        PhotonPoseEstimator photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, cam, robotToCam);
-        photonPoseEstimator.setReferencePose(prevEstimatedRobotPose);
-        
-        pose = photonPoseEstimator.update();
-        
+    public static void initiatePose() throws IOException {
+    }
 
-}
+    public static Pose2d getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) throws IOException {
+
+        photonPoseEstimator.setReferencePose(prevEstimatedRobotPose);
+
+        Optional<EstimatedRobotPose> pose = photonPoseEstimator.update();
+        return pose.get().estimatedPose.toPose2d();
+    }
 
 }
