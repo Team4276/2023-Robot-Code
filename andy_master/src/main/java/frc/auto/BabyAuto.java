3dd65c4c3a60657cf7a99ee7f2c0f8d55ed313a5
acappon1 <acappon1@hotmail.com>
2023-04-13 10:07:47 -0700

    Revert 'master' to back out recent changes to go back to "Fixed Elbow pid"

--- andy_master/src/main/java/frc/auto/BabyAuto.java
+++ andy_master/src/main/java/frc/auto/BabyAuto.java
@@ -1,201 +1,114 @@
 package frc.auto;
 
-import frc.systems.Intake;
-import frc.systems.PIDElbow;
+import frc.robot.Robot;
 import frc.systems.TeleopDrivetrain;
-import frc.systems.Intake.intake;
-import frc.systems.PIDElbow.Pos;
-import frc.utilities.Gyroscope;
-import frc.utilities.RobotMode;
 import frc.utilities.SoftwareTimer;
-import frc.utilities.RobotMode.ROBOT_MODE;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class BabyAuto {
     private static final double LEFTMOBILITYTIME = 4.5; // time in seconds
+    private static final double RIGHTMOBILITYTIME = 2.5; // time in seconds
+    private static final double MIDDLEBALANCTIME1 = 0.5; // initial back away
     private static final double MIDDLEBALANCTIME2 = 2.5; // drive up onto platform
+    private static final double MIDDLEBALANCTIME3 = 0; // delay until arm goes down
     public static final double MOTORPOWER = 0.20;
+    private static final double DEADZONEANGLE = 5; // deg
 
-    private static final double FASTZONEPOWER = 0.2;
-    private static final double SLOWZONE = 10;
-    private static final double SLOWZONEPOWER = 0.1;
-    private static final double DEADZONE = 2;
-    private static double desired_angle = 0;
+    public static boolean balance = false;
 
-    private static SoftwareTimer timer1;
-    private static SoftwareTimer timer2;
-    private static SoftwareTimer timer3;
-    private static SoftwareTimer timer4;
+    private static SoftwareTimer timer;
+    private static SoftwareTimer middleTimer;
+    private static SoftwareTimer middleArmDelayTimer;
+    private static SoftwareTimer timer4_GOD_IGOTTASTOPADDINSOMANYTIMERS;
 
-    private static boolean firstRun1 = true;
-    private static boolean firstRun2 = true;
-    private static boolean firstRun3 = true;
-    private static boolean firstRun4 = true;
-    private static boolean firstRun5 = true;
-    private static boolean firstrun6 = true;
-
+    private static boolean firstRun = true;
+    private static boolean firstRunTimer = true;
+    private static boolean firstRunTimer3_holy_cow_we_have_too_many_logic_variables_for_this_code_i_ned_to_fix = true;
 
     public static boolean taskIsFinished = false;
 
-    public static AUTO_MOBILITY_MODE myAutoMobilityMode;
-
-    public static enum AUTO_MOBILITY_MODE{
-        FORWARD,
-        BACKWARD,
-        NOPOWER,
-        PICKUP
-    }
-
-    public static AUTO_MOBILITY_MODE get() {
-        return myAutoMobilityMode;
-    }
-    
-    public static void set(AUTO_MOBILITY_MODE val) {
-        myAutoMobilityMode = val;
-    }
-
-    public static String getString() {
-
-        switch (myAutoMobilityMode.ordinal()) {
-          case 0:
-            return "FORWARD";
-          case 1:
-            return "BACKWARD";
-          case 2:
-            return "NOPOWER";
-          case 3:
-            return "TURNING";
-    
-          default:
-            break;
-        }
-        return "*****";
-      }
+    public static boolean usingDrivetrainMotorsForward = false;
+    public static boolean usingDrivetrainMotorsBackward = false;
+    public static boolean usingDrivetrainMotorsNOPOWER = false;
 
     public static void BabyAutoInit(){
-        timer1 = new SoftwareTimer();
-        timer2 = new SoftwareTimer();
-        timer3 = new SoftwareTimer();
-        timer4 = new SoftwareTimer();
-
-        myAutoMobilityMode = AUTO_MOBILITY_MODE.NOPOWER;
-
+        timer = new SoftwareTimer();
+        middleTimer = new SoftwareTimer();
+        middleArmDelayTimer = new SoftwareTimer();
+        timer4_GOD_IGOTTASTOPADDINSOMANYTIMERS = new SoftwareTimer();
     }
 
-    public static boolean ScoreMobility(){
-        RobotMode.set(ROBOT_MODE.AUTO_DRIVING);
-
-        if (firstRun1){
-            timer1.setTimer(LEFTMOBILITYTIME);
-            firstRun1 = false;
+    public static void leftScoreMobility(){
+        if (firstRun){
+            timer.setTimer(LEFTMOBILITYTIME);
+            firstRun = false;
         }
 
-        if (timer1.isExpired()){
-            set(AUTO_MOBILITY_MODE.NOPOWER);
+        if (timer.isExpired()){
+            usingDrivetrainMotorsNOPOWER = true;
             taskIsFinished = true;
-
-            firstRun1 = true;
-
-            return true;
-
         } else {
-            set(AUTO_MOBILITY_MODE.BACKWARD);
-
-            return false;
+            usingDrivetrainMotorsBackward = true;
         }
     }
 
     public static void middleBalance(boolean forward){
-        RobotMode.set(ROBOT_MODE.AUTO_DRIVING);
-
-        if (firstRun1){
-            timer1.setTimer(MIDDLEBALANCTIME2);
-            firstRun1 = false;
+        if (firstRun){
+            timer.setTimer(MIDDLEBALANCTIME2);
+            //middleArmDelayTimer.setTimer(MIDDLEBALANCTIME3);
+            firstRun = false;
         }
 
-        if (timer1.isExpired()){
-            if (firstRun2){
-                timer2.setTimer(0.5);
-                firstRun2 = false;
+        /*if (middleArmDelayTimer.isExpired()){
+            if (firstRunTimer){
+                middleTimer.setTimer(MIDDLEBALANCTIME4);
+            }
+
+            PIDElbow.setPoint_Elbow = PIDElbow.DPAD_DOWN_ELBOW_COLLECT;
+        }*/
+
+        if (timer.isExpired()){
+            if (firstRunTimer3_holy_cow_we_have_too_many_logic_variables_for_this_code_i_ned_to_fix){
+                timer4_GOD_IGOTTASTOPADDINSOMANYTIMERS.setTimer(0.5);
+                firstRunTimer3_holy_cow_we_have_too_many_logic_variables_for_this_code_i_ned_to_fix = false;
 
             }
 
-            if (timer2.isExpired()){  
-                RobotMode.set(ROBOT_MODE.AUTO_BALANCING);
+            if (timer4_GOD_IGOTTASTOPADDINSOMANYTIMERS.isExpired()){  
 
-
+                balance = true;
             } else {
-                set(AUTO_MOBILITY_MODE.NOPOWER);
+                usingDrivetrainMotorsNOPOWER = true;
+                Robot.isTeleop = true;
             }
+
+            
+            SmartDashboard.putBoolean("isTeleop", Robot.isTeleop);
 
         } else {
             if (forward) {
-                set(AUTO_MOBILITY_MODE.FORWARD);
+                usingDrivetrainMotorsForward = true;
             } else if(!forward){
-                set(AUTO_MOBILITY_MODE.BACKWARD);
+                usingDrivetrainMotorsBackward = true;
             }
 
         }
-        
     }
 
-    public static boolean pickup(){
-        if (firstRun3){
-            desired_angle = Gyroscope.GetCorrectedYaw() + 180;
-            firstRun3 = false;
+
+    public static void rightScoreMobility(){
+        if (firstRun){
+            timer.setTimer(RIGHTMOBILITYTIME);
+            firstRun = false;
         }
 
-        if (Math.abs(desired_angle - Gyroscope.GetCorrectedYaw()) > SLOWZONE){
-            TeleopDrivetrain.assignMotorPower(FASTZONEPOWER, FASTZONEPOWER);
-            firstRun4 = true;
-            return false;
-        } else if (Math.abs(desired_angle - Gyroscope.GetCorrectedYaw()) > DEADZONE){
-            firstRun4 = true;
-            TeleopDrivetrain.assignMotorPower(SLOWZONEPOWER, SLOWZONEPOWER);
-            return false;
+        if (timer.isExpired()){
+            TeleopDrivetrain.assignMotorPower(0,0);
+            taskIsFinished = true;
         } else {
-            TeleopDrivetrain.assignMotorPower(0, 0);
-            if (firstRun4){
-                if (collect()){
-                    firstRun3 = true;
-                    firstRun4 = false;
-                }
-                return false;
-
-            } else {
-                return true;
-            }
-
+            TeleopDrivetrain.assignMotorPower(MOTORPOWER,-1*MOTORPOWER);
         }
 
-
-    }
-
-    public static boolean collect(){
-        if (firstRun5){
-            timer3.setTimer(0.5);
-        }
         
-        if (timer3.isExpired()){
-            if (firstrun6){
-                timer4.setTimer(0.25);
-            
-            }
-
-            if (timer4.isExpired()){
-                Intake.intakeState = intake.OFF;
-                PIDElbow.position = Pos.STOW;
-            } else {
-                Intake.intakeState = intake.INTAKE;
-            }
-
-
-            firstRun5 = true;
-            return true;
-        } else {
-            PIDElbow.position = Pos.COLLECT;
-
-            return false;
-        }
-
     }
 }
--- andy_master/src/main/java/frc/utilities/Gyroscope.java
+++ andy_master/src/main/java/frc/utilities/Gyroscope.java
@@ -2,16 +2,10 @@ package frc.utilities;
 
 import edu.wpi.first.wpilibj.ADIS16470_IMU;
 
+import frc.robot.Robot;
+
 public class Gyroscope {
     public static final ADIS16470_IMU imu = new ADIS16470_IMU();
-
-    public static double initialPitch = 0;
-    public static double initialYaw = 0;
-
-    public static void gyroscopeInit(){
-        initialPitch = GetPitch();
-        initialYaw = GetYaw();
-    }
 
     static public double GetYaw() {
 
@@ -27,21 +21,12 @@ public class Gyroscope {
         return imu.getYComplementaryAngle();
     }
 
-    static public double GetCorrectPitch() {
+    static public double GetCorrectPitch(double currentPitch) {
         double Pitch = imu.getYComplementaryAngle();
 
-        Pitch -= initialPitch;
+        Pitch = currentPitch - Robot.initialPitch;
 
         return Pitch;
-    }
-
-    static public double GetCorrectedYaw(){
-        double Yaw = imu.getAngle() % 360;
-
-        Yaw -= initialYaw;
-
-        return Yaw;
-
     }
 
 }
