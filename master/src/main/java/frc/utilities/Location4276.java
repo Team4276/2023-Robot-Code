58f1b1d310d14f77ea83f92ba5480bc4fadacadd
acappon1 <acappon1@hotmail.com>
2023-04-13 08:03:34 -0700

    PhotonVision is working well enough to move to master. When Apriltags are visible, Location4276 is continuously updated with field position in meters.  (0,0,0) origin is at center of field, X axis is down the centerline of he field, Y is the dividing line between red and blue sides.  The Blue substations are in the quadrant where both X and Y are positive.

--- master/src/main/java/frc/utilities/Location4276.java
+++ master/src/main/java/frc/utilities/Location4276.java
@@ -1,14 +1,16 @@
 package frc.utilities;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 import frc.systems.BaseDrivetrain;
 
 public class Location4276 {
 
-    // Limelight
-    private static Vector3 v3Limelight;
-    private static Vector3 v3PreviousLimelight;
+    // PhotonVision
+    private static Vector3 v3PhotonVision;
+    private static Vector3 v3PreviousPhotonVision;
 
     private static Vector3 v3Position;
     private static Vector3 v3PrevPosition;
@@ -19,63 +21,56 @@ public class Location4276 {
     private static double posFixErrorCorrection = 0.0;
 
     private static double gyroCorrection = 0.0;
-    private static long positionUpdateTimeMillisecs;
+    private static double prevPosition = 0.0;
 
     public Location4276() {
 
-        v3Limelight = new Vector3(0.0, 0.0, 0.0);
-        v3PreviousLimelight = new Vector3(0.0, 0.0, 0.0);
+        v3PhotonVision = new Vector3(0.0, 0.0, 0.0);
+        v3PreviousPhotonVision = new Vector3(0.0, 0.0, 0.0);
         v3Position = new Vector3(0.0, 0.0, 0.0);
         v3PrevPosition = new Vector3(0.0, 0.0, 0.0);
 
-        checkLimelightRobotPosition();
+        final double convertOneRevolutionToFeet = 0.1879;
+        final double convertFeetToMeters = 0.3048;
+
+        double kNeoPositionConversionFactor = convertOneRevolutionToFeet;
+        kNeoPositionConversionFactor *= convertFeetToMeters;
+
+        BaseDrivetrain.flDriveX.getEncoder().setPositionConversionFactor(kNeoPositionConversionFactor);
+        BaseDrivetrain.frDriveX.getEncoder().setPositionConversionFactor(kNeoPositionConversionFactor);
     }
 
-    public void checkLimelightRobotPosition() {
-
-        final double feet_per_meter = 3.28084;
-
-        double[] errorhandle = new double[6];
-        double[] positionLimelight = Robot.ntLimelight.getEntry("botpos").getDoubleArray(errorhandle);
-        double x = positionLimelight[0];
-        double y = positionLimelight[1];
-        double z = positionLimelight[2];
-
-        // position units are meters at this point - convert to feet
-        x *= feet_per_meter;
-        y *= feet_per_meter;
-        z *= feet_per_meter;
-
-        v3Limelight.set(x, y, z);
+    public Pose2d getRobotPosition() {
+        Rotation2d rot2d = Rotation2d.fromDegrees(heading);
+        return new Pose2d(v3Position.x, v3Position.y, rot2d);
     }
 
     public boolean isNewPositionFix() {
-        return (!v3Limelight.isEqual(v3PreviousLimelight));
+        return (!v3PhotonVision.isEqual(v3PreviousPhotonVision));
     }
 
     public void setPositionFix() {
-        v3PreviousLimelight.copy(v3Limelight);
-        v3Position.copy(v3Limelight);
+        v3PreviousPhotonVision.copy(v3PhotonVision);
+        v3Position.copy(v3PhotonVision);
     }
 
-    private double getHeading() {
-        return Gyroscope.GetYaw() - gyroCorrection;
+    public static double getRawHeading() {
+        double retVal = Gyroscope.GetYaw(); // starts at zero, negative is CCW from top,
+        retVal %= 360; // now in range -360 to +360
 
-        // Robot coordinates reported by Limelight
-        // 3d Cartesian Coordinate System with (0,0,0) located at the center of the
-        // robot's frame projected down to the floor.
-        // X Pointing forward (Forward Vector)
-        // Y Pointing toward the robot's right (Right Vector)
-        // Z Pointing upward (Up Vector)
+        if (retVal > 180) {
+            retVal -= 360;
+        }
+        if (retVal < -180) {
+            retVal += 360;
+        }
+        return retVal;
+    }
 
-        // This is different from the coordinate system used by the Gyro (X and Y axis
-        // swapped)
-        // X Pointing toward the robot's right (Right Vector)
-        // Y Pointing forward (Forward Vector)
-        // Z Pointing upward (Up Vector)
-
-        // I am guessing that it doesn't matter for heading because rotation is around
-        // the Z-axis which is same for both
+    private static double getHeading() {
+        double retVal = getRawHeading();
+        retVal += gyroCorrection;
+        return retVal;
     }
 
     private double getDistanceTo(Vector3 otherPos) {
@@ -87,25 +82,15 @@ public class Location4276 {
         // Find the lowest speed encoder (assume faster speed indicates slippage or
         // minor difference due to turning)
 
+        double FRencoderSpeed = BaseDrivetrain.flDriveX.getEncoder().getVelocity();
+        double FLencoderSpeed = BaseDrivetrain.frDriveX.getEncoder().getVelocity();
+
         double rpmSpeed = 10000000.0;
-
-        double BRencoder = BaseDrivetrain.brDriveX.getEncoder().getVelocity();
-        double BLencoder = BaseDrivetrain.blDriveX.getEncoder().getVelocity();
-        double FRencoder = BaseDrivetrain.flDriveX.getEncoder().getVelocity();
-        double FLencoder = BaseDrivetrain.frDriveX.getEncoder().getVelocity();
-
-        // 2023 robot can sense velocity directly from the SparkMAX controller
-        if (rpmSpeed > FRencoder) {
-            rpmSpeed = FRencoder;
+        if (rpmSpeed > FRencoderSpeed) {
+            rpmSpeed = FRencoderSpeed;
         }
-        if (rpmSpeed > (-1 * BRencoder)) { 
-            rpmSpeed = (-1 * BRencoder);
-        }
-        if (rpmSpeed > (-1 * FLencoder)) {
-            rpmSpeed = (-1 * FLencoder);
-        }
-        if (rpmSpeed > BLencoder) {
-            rpmSpeed = BLencoder;
+        if (rpmSpeed > (-1 * FLencoderSpeed)) {
+            rpmSpeed = (-1 * FLencoderSpeed);
         }
 
         // Speed units are rpm at this point - need to convert to feet/sec:
@@ -113,58 +98,77 @@ public class Location4276 {
         // (100 rotations)/(8.5 gearbox redux) * (2pi*3.05in) = 225.5in, or 18.79ft/100
         // rotations
         final double convertRpmToFeet = 0.1879;
+        final double convertFeetToMeters = 0.3048;
         rpmSpeed *= convertRpmToFeet;    // feet/min
-        rpmSpeed /= 60.0;                // feet/sec
+        rpmSpeed *= convertFeetToMeters; // meters/min
+        rpmSpeed /= 60.0; // meters/sec
 
         return rpmSpeed;
     }
 
-    private boolean isMotionSufficientToEstimateHeading() {
-        return (getEncoderSpeed() > 1.0); // feet/sec
+    public double getEncoderDistance(double prevPos) {
+        // Find the lowest speed encoder (assume faster speed indicates slippage or
+        // minor difference due to turning)
+
+        double FRencoderPosition = BaseDrivetrain.flDriveX.getEncoder().getPosition();
+        double FLencoderPosition = BaseDrivetrain.frDriveX.getEncoder().getPosition();
+
+        double pos = 10000000.0;
+        if (pos > FRencoderPosition) {
+            pos = FRencoderPosition;
+        }
+        if (pos > (-1 * FLencoderPosition)) {
+            pos = (-1 * FLencoderPosition);
+        }
+
+        return pos - prevPos;
     }
 
     public void updatePosition() {
 
-        checkLimelightRobotPosition();
+        FieldPose.updatePosition();
+
+        if (FieldPose.isValidPosition) {
+            v3PhotonVision.x = FieldPose.position.getX();
+            v3PhotonVision.y = FieldPose.position.getY();
+
+            // Average gyro correction over 10 samples
+            gyroCorrection *= 0.9;
+            gyroCorrection += ((FieldPose.getRotationDegrees() - getRawHeading()) / 10.0);
+
+            SmartDashboard.putNumber("PhotonVision_X: ", v3PhotonVision.x);
+            SmartDashboard.putNumber("PhotonVision_Y: ", v3PhotonVision.y);
+            SmartDashboard.putNumber("RawGyro: ", Gyroscope.GetYaw());
+            SmartDashboard.putNumber("getRawHeading: ", getRawHeading());
+            SmartDashboard.putNumber("FieldPose.getRotationDegrees: ", FieldPose.getRotationDegrees());
+            SmartDashboard.putNumber("gyroCorrection: ", gyroCorrection);
+            SmartDashboard.putNumber("EncoderPosition: ", BaseDrivetrain.flDriveX.getEncoder().getPosition());
+        }
 
         if (isNewPositionFix()) {
-            posFixErrorCorrection = getDistanceTo(v3Limelight);
+            posFixErrorCorrection = getDistanceTo(v3PhotonVision);
             setPositionFix();
 
+            prevPosition = getEncoderDistance(prevPosition);
+
         } else {
-            // Limelight did not find any Apriltag
+            // PhotonVision did not find any Apriltag
             posFixErrorCorrection = 0.0;
 
-            if (isMotionSufficientToEstimateHeading()) {
-                // heading of robot moving from previous position to current position
-                // Y axis == Robot Forward, X == Robot right
-                // 0.0 heading == Robot forward, Positive rotation to Robot Right, range -180.0
-                // to +180.0
-                double estimateCourseMadeGood = v3PrevPosition.angle(v3Position);
-                gyroCorrection = estimateCourseMadeGood - Gyroscope.GetYaw();
-            }
-
             // Extrapolate current position from previous position
-
-            long prevTimeMillisecs = positionUpdateTimeMillisecs;
-            positionUpdateTimeMillisecs = java.lang.System.currentTimeMillis();
-            long deltaTimeMillisecs = (positionUpdateTimeMillisecs - prevTimeMillisecs);
-
             heading = getHeading();
-            speed = getEncoderSpeed();
-            distance = speed * (deltaTimeMillisecs/1000.0);
-
-            SmartDashboard.putNumber("distance", distance);
-            SmartDashboard.putNumber("DektaTimeMillis", deltaTimeMillisecs);
+            distance = getEncoderDistance(prevPosition);
+            prevPosition = distance;
 
             v3PrevPosition.copy(v3Position);
 
-            v3Position.x = distance * Math.sin(heading);
-            v3Position.y = distance * Math.cos(heading);
-        }
+            v3Position.x += distance * Math.sin(heading);
+            v3Position.y += distance * Math.cos(heading);
 
-        if (getEncoderSpeed() != 0.0) { // No point in filling the log with duplicate data, timestanp will show periods
-                                       // of stillness
+            if (distance != 0.0) { // No point in filling the log with duplicate data, timestanp will show when
+                                   // motionless
+
+                try {
             Robot.myLogFile.write(String.valueOf(posFixErrorCorrection));
             Robot.myLogFile.write(String.valueOf(","));
             Robot.myLogFile.write(String.valueOf(System.currentTimeMillis()));
@@ -181,6 +185,10 @@ public class Location4276 {
             Robot.myLogFile.write(String.valueOf(","));
             Robot.myLogFile.write(String.valueOf(v3Position.z));
             Robot.myLogFile.write(String.valueOf("\r\n"));
+                } catch (Exception e) {
+
+                }
+            }
         }
     }
 
